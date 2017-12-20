49070ed0c9594304f1bf58690d4fb3b147eac69b
mjr52 <mjr52@duke.edu>
2017-12-20 09:14:00 -0500

    Added my sample example

---
+++ examples/mjr52_gauss_example/gen_mesh_bc.py
@@ -0,0 +1,19 @@
+def main():
+    from fem.mesh import GenMesh
+    from fem.mesh import bc
+
+    xyz = (-1.5, 0.0, 0.0, 1.5, -3.0, 0.0)
+    numElem = (75, 75, 150)
+    GenMesh.run(xyz, numElem)
+
+    # setup quarter symmetry condition
+    pml_elems = ((5, 0), (0, 5), (5, 5))
+    face_constraints = (('1,1,1,1,1,1', '1,0,0,0,1,1'),
+                        ('0,1,0,1,0,1', '1,1,1,1,1,1'),
+                        ('1,1,1,1,1,1', '1,1,1,1,1,1'))
+    edge_constraints = (((0, 1), (1, 0), (0, 0)), '1,1,0,1,1,1')
+
+    bc.apply_pml(pml_elems, face_constraints, edge_constraints)
+
+if __name__ == "__main__":
+    main()
